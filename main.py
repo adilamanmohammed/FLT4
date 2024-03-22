c74fc665e61d7eb9da74a0da706e3ead039c8696
Adil Aman Mohammed <adil.mohammed@okstate.edu>
2024-03-22 00:04:21 -0500

    adil edited

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ Name : Adil Aman Mohammed
 Course : Formal language theory
 Assignment no: 5
 CWID : A20395630
-Description: the below code is an implementation of taking input CFG and applying 3 algorithms (Remove immediate left recursion, Remove left recursion, Remove unit productions)
+Description:   the below    code is an implementation of taking input CFG and applying 3 algorithms (Remove immediate left recursion, Remove left recursion, Remove unit productions)
 """
 
 import sys
